67cff4aead503b849a6dfbea1fafa263ffc9388d
Sudeep Rao <sudeep.rao619@gmail.com>
2023-12-04 20:09:18 +0530

    Initial Commit

---
+++ ChartsExp/ChartsExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExpApp.swift
+//  ChartsExp
+//
+//  Created by Sudeep Rao on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
